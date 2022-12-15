4a5d9616c34c409bc19e4d72f89311483d34f065
Angelina <zhuravskayalina@gmail.com>
2022-12-16 00:09:12 +0400

    feat: implement all date task functions

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -12,15 +12,15 @@
  * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
  *
  * @param {string} value
- * @return {date}
+ * @return {Date}
  *
  * @example:
  *    'December 17, 1995 03:24:00'    => Date()
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -28,14 +28,14 @@ function parseDataFromRfc2822(/* value */) {
  * For ISO 8601 date specification refer to : https://en.wikipedia.org/wiki/ISO_8601
  *
  * @param {string} value
- * @return {date}
+ * @return {Date}
  *
  * @example :
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -44,7 +44,7 @@ function parseDataFromIso8601(/* value */) {
  * Please find algorithm here: https://en.wikipedia.org/wiki/Leap_year#Algorithm
  *
  * @param {date} date
- * @return {bool}
+ * @return {boolean}
  *
  * @example :
  *    Date(1900,1,1)    => false
@@ -53,8 +53,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  return (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0;
 }
 
 
@@ -62,8 +63,8 @@ function isLeapYear(/* date */) {
  * Returns the string representation of the timespan between two dates.
  * The format of output string is "HH:mm:ss.sss"
  *
- * @param {date} startDate
- * @param {date} endDate
+ * @param {Date} startDate
+ * @param {Date} endDate
  * @return {string}
  *
  * @example:
@@ -73,10 +74,31 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
-}
+function timeSpanToString(startDate, endDate) {
+  function formatDate(data) {
+    return data < 9 ? `0${data}` : data;
+  }
 
+  function formatMs(data) {
+    let result;
+    if (data < 9) {
+      result = `00${data}`;
+    } else if ((data > 9) && (data < 99)) {
+      result = `0${data}`;
+    } else {
+      result = data;
+    }
+    return result;
+  }
+
+  const difference = endDate.getTime() - startDate.getTime();
+  const hours = Math.floor(difference / 3600000);
+  const min = Math.floor((difference - hours * 3600000) / 60000);
+  const sec = Math.floor((difference - (hours * 3600000 + min * 60000)) / 1000);
+  const ms = difference - (hours * 3600000 + min * 60000 + sec * 1000);
+  const result = `${formatDate(hours)}:${formatDate(min)}:${formatDate(sec)}.${formatMs(ms)}`;
+  return result;
+}
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
@@ -85,7 +107,7 @@ function timeSpanToString(/* startDate, endDate */) {
  *
  * SMALL TIP: convert to radians just once, before return in order to not lost precision
  *
- * @param {date} date
+ * @param {Date} date
  * @return {number}
  *
  * @example:
@@ -94,8 +116,22 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hours = date.getUTCHours() >= 12 ? date.getUTCHours() - 12 : date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const hoursDeg = 30;
+  const minForHoursDeg = 0.5;
+  const minDeg = 6;
+
+  const hoursAngle = hours * hoursDeg + minutes * minForHoursDeg;
+  const minutesAngle = minutes * minDeg;
+  let angleBetween = Math.abs(hoursAngle - minutesAngle);
+  if (angleBetween === 180) return Math.PI;
+  if (angleBetween > 180) {
+    angleBetween = Math.abs(360 - angleBetween);
+  }
+
+  return (angleBetween * Math.PI) / 180;
 }
 
 
