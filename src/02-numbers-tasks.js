02b221ccacda2ba92dea90fcaa0210c2652ef5e9
Angelina <zhuravskayalina@gmail.com>
2022-12-31 14:54:34 +0400

    feat: implement all number task functions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -237,8 +237,8 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number(value) ? Number(value) : def;
 }
 
 module.exports = {
